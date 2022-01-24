14f8de30e2e031382f42f490bcd8784cc27ef6dd
GeethanjaliReddy20 <98315646+GeethanjaliReddy20@users.noreply.github.com>
2022-01-24 19:25:58 +0530

    Add files via upload

---
+++ even/even/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace even
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the input");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("given number is even");
+            }
+            else
+                Console.WriteLine("given number is odd");
+        }
+    }
+}
