54e6857548b20d514f0809bcd98f359ca7716a3d
GeethanjaliReddy20 <98315646+GeethanjaliReddy20@users.noreply.github.com>
2022-01-24 19:24:49 +0530

    Add files via upload

---
+++ greeting/greeting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace greeting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string username=Console.ReadLine();
+           Console.WriteLine($"Hi! {username}");
+           Console.WriteLine("Welcome to the world of c#");
+        }
+    }
+}
