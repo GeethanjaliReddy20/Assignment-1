51ac54b3f4da1083d7be6eb3fb81ab53632e9195
GeethanjaliReddy20 <98315646+GeethanjaliReddy20@users.noreply.github.com>
2022-01-24 19:19:16 +0530

    Add files via upload

---
+++ smallestlargest/smallestlargest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace smallestlargest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("largest and smallest number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (a > b && a > c)
+            {
+                Console.WriteLine($"The largest number is{a}");
+            }
+            else if (b > a && b > c)
+                Console.WriteLine($"The largest number is{b}");
+            else
+                Console.WriteLine($"The largest numebr is{c}");
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine($"The  smallest number is{a}");
+            }
+            else if (b < a && b < c)
+                Console.WriteLine($"The  smallest number is{b}");
+            else
+                Console.WriteLine($"The smallest numebr is{c}");
+        }
+    }
+}
