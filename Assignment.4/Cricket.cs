329d5c0c504f3700194620506e289109cc70b509
GeethanjaliReddy20 <98315646+GeethanjaliReddy20@users.noreply.github.com>
2022-02-03 13:04:30 +0530

    Add files via upload

---
+++ Assignment.4/Cricket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cricket
+{
+    class Cricket
+    {
+        int sum = 0, avg;
+
+        public void pointsCalculation(int no_of_matches)
+        {
+            int[] Score = new int[20];
+            for (int i = 0; i < no_of_matches; i++)
+            {
+                Console.WriteLine("Enter Match{0} score :", i + 1);
+                Score[i] = Convert.ToInt32(Console.ReadLine());
+                sum += Score[i];
+            }
+            avg = sum / no_of_matches;
+            Console.WriteLine("Sum:{0} and Avg:{1} for IPL Scores in {2} matches!!", sum, avg, no_of_matches);
+
+
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Cricket c = new Cricket();
+            Console.WriteLine("Enter no of matches:");
+            x = Convert.ToInt32(Console.ReadLine());
+            c.pointsCalculation(x);
+            Console.Read();
+        }
+    }
+}
+    
---
+++ Assignment.4/Products.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID
+{
+
+    class Product
+    {
+
+        Dictionary<string, Double> Products = new Dictionary<string, double>();
+        public void GetData()
+        {
+            Console.WriteLine("Enter 3 Products : ");
+            for (int i = 0; i < 3; i++)
+            {
+                Products.Add(Console.ReadLine(), double.Parse(Console.ReadLine()));
+            }
+        }
+        public void Sortdata()
+        {
+            Console.WriteLine("After Sorting the Product by Price :");
+            foreach (KeyValuePair<string, Double> Price in Products.OrderBy(key => key.Value))
+            {
+                Console.WriteLine("Product: {0}, Value: {1}", Price.Key, Price.Value);
+            }
+        }
+    }
+
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+            p.GetData();
+            p.Sortdata();
+        }
+    }
+
+
+}
+
+
+
+        
