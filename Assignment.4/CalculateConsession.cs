5e94e1d09618fb528a09913db7ec14044c001574
GeethanjaliReddy20 <98315646+GeethanjaliReddy20@users.noreply.github.com>
2022-02-03 12:47:46 +0530

    Add files via upload

---
+++ Assignment.4/CalculateConsession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculateConsession;
+
+namespace CalculateConsession
+{
+    class Program224
+    {
+        static void Main()
+        {
+            int TotalFare = 500;
+            string Name;
+            int Age;
+            Console.Write("Enter No. of Tickets You Want to Book: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter Citizen Name: ");
+                Name = Console.ReadLine();
+                Concession c = new Concession();
+                c.CalculateConcession1();
+
+            }
+
+        }
+    }
+}
